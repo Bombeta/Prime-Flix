e0f96b50cbfad61dac55dce1d03fa28163cda8fa
Bombeta <paullo_jf@hotmail.com>
2022-09-20 23:03:35 -0300

    listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,14 @@
 import{useEffect, useState} from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import "./style.css"
 
 // URL da API: movie/now_playing?api_key=3f8a34de060d36f77d236dfda2afe1f8&language=pt-BR
 
 function Home(){
 
     const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(()=>{
         async function loadFilmes(){
@@ -17,16 +20,37 @@ function Home(){
               }  
             })
             
-            console.log(response.data.results);
+            setFilmes(response.data.results.slice(0,10));
+            
+            //console.log(filmes);
         }
 
         loadFilmes();
+        setLoading(false);
     }, []);
 
 
+    if(loading){
+        return(
+            <div className="loading">
+                <h2>Carregando filmes...</h2>
+            </div>
+        )
+    }
+
     return(
-        <div>
-            <h2>Home</h2>
+        <div className="container">
+            <div className="lista-filmes">
+              {filmes.map((filmes) => {
+                return(
+                    <article key={filmes.id}>
+                        <strong>{filmes.title}</strong>
+                        <img src={`https://image.tmdb.org/t/p/original/${filmes.poster_path}`} alt={filmes.title}></img>
+                        <Link to={`/filmes/${filmes.id}`}>Acessar</Link>
+                    </article>
+                )
+              })}
+            </div>
         </div>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header';
+import Erro from './pages/Erro';
 
 
 function RoutesApp(){
@@ -11,7 +12,9 @@ function RoutesApp(){
             <Header/>
             <Routes>
                 <Route path="/" element={<Home/>} />
-                <Route path="/filme/:id" element={<Filme/>} />
+                <Route path="/filmes/:id" element={<Filme/>} />
+                
+                <Route path="*" element={<Erro/>} />
             </Routes>
         </BrowserRouter>
     )

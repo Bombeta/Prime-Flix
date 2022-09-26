0f100792128a4b797d36149c582881a95fae54a7
Bombeta <paullo_jf@hotmail.com>
2022-09-26 18:16:18 -0300

    finalizando o projeto

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,55 @@
+import { useEffect, useState } from 'react';
+import "./favoritos.css";
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 
 function Favoritos() {
+
+    const [filmes, setFilmes] = useState([]);
+    
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeFlix");
+        setFilmes(JSON.parse(minhaLista) || []);
+    
+    }, []);
+
+    function excluirFilme(id){
+        
+        let filtrosFilmes = filmes.filter((item)=> {
+            return (item.id !== id);
+        })
+
+        setFilmes(filtrosFilmes);
+
+        localStorage.setItem("@primeFlix", JSON.stringify(filtrosFilmes));
+        toast.success("Filme removido com sucesso!");
+    }
+
     return(
-        <h2>Favoritos</h2>
+        <div className='meus-filmes'>
+            <h2>Minha lista</h2>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
+
+            <ul>
+               {filmes.map((item)=> {                   
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+                            <div>
+                                <Link to={`/filmes/${item.id}`}>Ver detalhes</Link>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
+                            </div>
+                        </li>                  
+                    )
+               })}
+            </ul> 
+        </div>
+        
     )
 }
 
 export default Favoritos;
+
--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,10 +1,14 @@
 import {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 import api from '../../services/api';
+import "./style.css"
 
 function Filme() {
     const { id } = useParams();
+    const navigate = useNavigate();  
+
     const [filme, setFilme] = useState();
     const [loading, setLoading] = useState(true);
 
@@ -17,12 +21,14 @@ function Filme() {
                 }
             })
             .then((response)=>{
-                //console.log(response.data);
+                console.log(response.data);
                 setFilme(response.data);
                 setLoading(false);
             })
             .catch(() => {
                 console.log("FILME NAO ENCONTRADO");
+                navigate("/", { replace: true });
+                return;
             })
         }
     
@@ -32,7 +38,24 @@ function Filme() {
         return() =>{
             console.log("COMPONENT FOI DESMONTADO!");
         }
-    },[])
+    },[navigate, id])
+
+    function salvarFilme(){
+        const minhaLista = localStorage.getItem("@primeFlix");
+
+        let filmesSalvos = JSON.parse(minhaLista) || [];
+
+        const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id);
+    
+        if(hasFilme){
+            toast.warn('ESTE FILME JÁ ESTÁ NA LISTA');
+            return;
+        }
+
+        filmesSalvos.push(filme);
+        localStorage.setItem("@primeFlix", JSON.stringify(filmesSalvos));
+        toast.success("FILME SALCO COM SUCESSO!");
+    }
 
     if(loading){
         return(
@@ -46,13 +69,22 @@ function Filme() {
     return(
 
       <div className="filmes-info">
-        <h1>{filme.tilte}</h1>       
+        <h1>{filme.title}</h1>       
         <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}></img>
       
         <h3>Sinopse</h3>
         <span>{filme.overview}</span>
 
         <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+        <div className='area-buttons'>
+            <button onClick={salvarFilme}>Salvar</button>
+            <button>
+                <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+                    Trailer
+                </a>
+            </button>
+        </div>
 
       </div>
         
